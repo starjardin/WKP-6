ecb4e69bfb842a66ec726519a2b0db9544a372f9
Tantely Andrianarivola <tantely.and@onja.org>
2020-08-08 08:26:05 +0300

    added plan for the first step

---
+++ script.js
@@ -0,0 +1,4 @@
+//create the ul and the food title and card container to the root html.
+//grab the ul to put the li form the array object
+//create an empty array or a state.
+//map throuh the array and push it to the dom.
